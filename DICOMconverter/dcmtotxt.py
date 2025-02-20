3aa71a0a24285d1bb55c3631d76b9eee21f08a1b
albertoarzenton <81620877+albertoarzenton@users.noreply.github.com>
2025-02-20 16:53:00 +0100

    Create dcmtotxt.py

---
+++ DICOMconverter/dcmtotxt.py
@@ -0,0 +1,21 @@
+#DICOM to TXT file converter
+
+#Author: A. Arzenton
+#Contact: alberto.arzenton@pd.infn.it
+
+
+from pydicom import dcmread
+import numpy as np
+
+image = dcmread("PET_18FDG_1.dcm")
+CT = image.pixel_array[0:147,0:159,0:159]
+
+sh = [CT.shape[0], CT.shape[1], CT.shape[2]]
+
+with open("PET_1.txt", "w") as output:
+    lines = []
+    for i in range(0, sh[2]):
+        for j in range(0, sh[1]):
+            for k in range(0, sh[0]):
+                lines.append(str(k)+" "+str(j)+" "+str(i)+" "+str(CT[k,j,i])+'\n')
+    output.writelines(lines)
