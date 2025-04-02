7f63a3d56178d6248936f53e3b89fb495d6a317e
albertoarzenton <81620877+albertoarzenton@users.noreply.github.com>
2025-04-02 15:18:48 +0200

    Update Parameters.hh
    Distinction between CT and PET or SPECT parameters and implementation of HU intervals.

--- include/Parameters.hh
+++ include/Parameters.hh
@@ -4,10 +4,24 @@
 #include "G4SystemOfUnits.hh"
 
 namespace Parameters {
+
+    // CT parameters
     constexpr G4int cVoxelX = 148;
     constexpr G4int cVoxelY = 160;
     constexpr G4int cVoxelZ = 160;
     constexpr G4double cVoxelSize = 0.5*mm;
+
+    // HU intervals
+    constexpr G4int HUminLung = -3100;
+    constexpr G4int HUminSoft = -200;
+    constexpr G4int HUminBone = 2900;
+
+    // PET or SPECT parameters
+    constexpr G4int eVoxelX = 148;
+    constexpr G4int eVoxelY = 160;
+    constexpr G4int eVoxelZ = 160;
+    constexpr G4double eVoxelSize = 0.5*mm;
+
 }
 
 #endif
