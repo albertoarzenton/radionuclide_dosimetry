e1f092d761abdc6a4c6f361a7a6d994bb8eefd8a
albertoarzenton <81620877+albertoarzenton@users.noreply.github.com>
2025-07-11 16:56:56 +0200

    Update dosemap_vis.py
    Cumulative activity is now used to retrieve the absorbed dose.

--- read/dosemap_vis.py
+++ read/dosemap_vis.py
@@ -13,15 +13,13 @@ import matplotlib.pyplot as plt
 image = dcmread("../DICOMconverter/CT_1.dcm") #CT
 CT = image.pixel_array[0:132, 16:156, 12:152] #[0:148, 0:159, 0:159] full image
 
-image = dcmread("../DICOMconverter/PET_18FDG_1.dcm") #PET or SPECT
-img = image.pixel_array[0:132, 16:156, 12:152]
+# image = dcmread("../DICOMconverter/PET_18FDG_1.dcm") #PET or SPECT
+# img = image.pixel_array[0:132, 16:156, 12:152]
 
 
 #Parameters ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-a = 0.5 #Voxel size in mm
-
-sh = img.shape
+sh = CT.shape
 c1, c2, c3 = int((sh[0]-1)/2), int((sh[1]-1)/2), int((sh[2]-1)/2) #Center
 
 
@@ -42,20 +40,11 @@ with open("dose_map.txt",'r') as gatefile:
 
     dose = np.array(dose)[0:132, 16:156, 12:152]
 
-dose = dose*1000 #From Gy/s to mGy/s
+dose = dose*1000 #From Gy to mGy
+events = 1e5 #Simulated events
+TCA = 4.8e8 #Total cumulated activity [Bq·s]
+dose = dose/events*TCA #Normalization to total cumulated activity
 #print(dose.shape)
-dosei = np.zeros((sh[1],sh[2]))
-for j in range(0, sh[1]):
-    for k in range(0, sh[2]):
-        dosei[j,k] = np.max(dose[:,j,k])
-dosej = np.zeros((sh[0],sh[2]))
-for i in range(0, sh[0]):
-    for k in range(0, sh[2]):
-        dosej[i,k] = np.max(dose[i,:,k])
-dosek = np.zeros((sh[0],sh[1]))
-for i in range(0, sh[0]):
-    for j in range(0, sh[1]):
-        dosek[i,j] = np.max(dose[i,j,:])
 
 
 #Visualization ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -66,31 +55,31 @@ max_tra = np.max([np.max(dose[int(c1*h1)]), np.max(dose[int(c1*h1)])])
 max_cor = np.max([np.max(dose[:,int(c2*h2)]), np.max(dose[:,int(c2*h2)])])
 max_sag = np.max([np.max(dose[:,:,int(c3*h3)]), np.max(dose[:,:,int(c3*h3)])])
 
-f, ax = plt.subplots(2,3)
+f, ax = plt.subplots(1,3)
 
-for i in range(0, 2):
-    ax[i,0].imshow(CT[int(c1*h1)], "bone")
-    ax[i,1].imshow(CT[:,int(c2*h2)], "bone")
-    ax[i,2].imshow(CT[:,:,int(c3*h3)], "bone")
+#for i in range(0, 2):
+ax[0].imshow(CT[int(c1*h1)], "bone")
+ax[1].imshow(CT[:,int(c2*h2)], "bone")
+ax[2].imshow(CT[:,:,int(c3*h3)], "bone")
 
-pos00 = ax[0,0].imshow(img[int(c1*h1)], "inferno", alpha=A)
-f.colorbar(pos00, ax=ax[0,0])
-pos01 = ax[0,1].imshow(img[:,int(c2*h2)], "inferno", alpha=A)
-f.colorbar(pos01, ax=ax[0,1])
-pos02 = ax[0,2].imshow(img[:,:,int(c3*h3)], "inferno", alpha=A)
-f.colorbar(pos02, ax=ax[0,2], label="[kBq/mL]")
-ax[0,1].set_title("F-18 PET (0.5 mm)")
+# pos00 = ax[0,0].imshow(img[int(c1*h1)], "inferno", alpha=A)
+# f.colorbar(pos00, ax=ax[0,0])
+# pos01 = ax[0,1].imshow(img[:,int(c2*h2)], "inferno", alpha=A)
+# f.colorbar(pos01, ax=ax[0,1])
+# pos02 = ax[0,2].imshow(img[:,:,int(c3*h3)], "inferno", alpha=A)
+# f.colorbar(pos02, ax=ax[0,2], label="[kBq/mL]")
+# ax[0,1].set_title("F-18 PET (0.5 mm)")
 
-pos10 = ax[1,0].imshow(dose[int(c1*h1)], "magma", vmax=max_tra, alpha=A)
-f.colorbar(pos10, ax=ax[1,0])
-pos11 = ax[1,1].imshow(dose[:,int(c2*h2)], "magma", vmax=max_cor, alpha=A)
-f.colorbar(pos11, ax=ax[1,1])
-pos12 = ax[1,2].imshow(dose[:,:,int(c3*h3)], "magma", vmax=max_sag, alpha=A)
-f.colorbar(pos12, ax=ax[1,2], label="[mGy/s]")
-ax[1,1].set_title("Dose-rate map")
+pos10 = ax[0].imshow(dose[int(c1*h1)], "magma", vmax=max_tra, alpha=A)
+f.colorbar(pos10, ax=ax[0])
+pos11 = ax[1].imshow(dose[:,int(c2*h2)], "magma", vmax=max_cor, alpha=A)
+f.colorbar(pos11, ax=ax[1])
+pos12 = ax[2].imshow(dose[:,:,int(c3*h3)], "magma", vmax=max_sag, alpha=A)
+f.colorbar(pos12, ax=ax[2], label="[mGy]")
+f.suptitle("Dose map")
 
-ax[1,0].set_xlabel("Transverse")
-ax[1,1].set_xlabel("Coronal")
-ax[1,2].set_xlabel("Sagittal")
+ax[0].set_xlabel("Transverse")
+ax[1].set_xlabel("Coronal")
+ax[2].set_xlabel("Sagittal")
 
 plt.show()
